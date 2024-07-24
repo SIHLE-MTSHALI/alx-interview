945fb82dff21e4b62fda8ddd0c6976a12d69c565
Sihle Mtshali <sihle.mtshali8@gmail.com>
2024-07-24 15:11:35 -0700

    Fix PEP 8 style issues in pascal_triangle function

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,6 +3,7 @@
 Pascal's Triangle
 """
 
+
 def pascal_triangle(n):
     """
     Returns a list of lists of integers representing the Pascal's triangle of n
@@ -17,5 +18,5 @@ def pascal_triangle(n):
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
         row.append(1)
         triangle.append(row)
-    
+
     return triangle
